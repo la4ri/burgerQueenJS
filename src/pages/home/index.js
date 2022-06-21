ef9acd28a2f776c97bb25f907a6fa5e259b6086d
Larissa Moreira <larissamoreiracarvalho@gmail.com>
2022-06-21 15:20:22 -0300

    logo

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,11 @@
 export default () => {
   const container = document.createElement('div');
+  container.classList.add('homeContainer');
+
   const template = `
-  <h1> Olá Mundo! </h1>
+  <div>
+  <img class="logoPirateria" src="/pages/style/LogoPirateria.png">
+  </div>
   `
 
   container.innerHTML = template;
